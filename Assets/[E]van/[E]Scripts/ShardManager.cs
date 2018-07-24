065d8c6b30714adb0fd4f274d2f7895b74ba9276
Unknown <s179060@students.aie.edu.au>
2018-07-23 17:59:27 -0700

    Most of it
    Smashed stuff, made basic level, hooked up models and textures

---
+++ Assets/[E]van/[E]Scripts/ShardManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShardManager : MonoBehaviour
+{
+    public ShatterScript[] shatterScripts;
+
+    public float impactThreshhold;
+    public float meshThiccness;
+
+    // Use this for initialization
+    void Start ()
+    {
+        shatterScripts = FindObjectsOfType<ShatterScript>();
+        foreach (ShatterScript scripts in shatterScripts)
+        {
+            scripts.impactThreshhold = impactThreshhold;
+            scripts.GetComponent<MeshCollider>().inflateMesh = true;
+            scripts.GetComponent<MeshCollider>().skinWidth = meshThiccness;
+
+        }
+	}
+
+}
---
+++ Assets/[E]van/[E]Scripts/ShatterScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterScript : MonoBehaviour
+{
+    public float impactThreshhold;
+
+    private bool _isShattered = false;
+
+    public Rigidbody rb;
+
+
+    public bool IsShattered
+    {
+        get
+        {
+            return _isShattered;
+        }
+
+        set
+        {
+            _isShattered = value;
+            CheckShattered();
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.rigidbody != null
+            &&
+            other.rigidbody.velocity.magnitude >= impactThreshhold)
+        {
+            IsShattered = true;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        //rb.detectCollisions = false;
+	}
+	
+    void CheckShattered()
+    {
+        if (IsShattered == true)
+        {
+            rb.isKinematic = false;
+            //rb.detectCollisions = true;
+        }
+        else
+        {
+            rb.isKinematic = true;
+            //rb.detectCollisions = false;
+        }
+    }
+}
